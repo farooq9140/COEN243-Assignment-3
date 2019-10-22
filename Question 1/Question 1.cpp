feda8c91256e480a1512d81bd0b72c1284c3cdf4
Kevin Prom <k_prom@live.concordia.ca>
2019-10-22 11:04:43 -0400

    Q1 Average complete

--- Question 1/Question 1.cpp
+++ Question 1/Question 1.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int maximum(int*, int);
 int minimum(int*, int);
+double average(int*, int);
 
 
 int main() {
@@ -25,6 +26,14 @@ int main() {
 
 	cout << "Maximum grade is: " << maximum(grades, gradeCount) << endl;
 	cout << "Minimum grade is: " << minimum(grades, gradeCount) << endl;
+	cout << "Grade Average is: " << average(grades, gradeCount) << endl;
+	cout << "Grade Median is: ";
+	cout << "Number of A Grades: ";
+	cout << "Number of B Grades: ";
+	cout << "Number of C Grades: ";
+	cout << "Number of D Grades: ";
+	cout << "Number of F Grades: ";
+	return 0;
 }
 
 int maximum(int* grades, int gradeCount) {
@@ -45,4 +54,12 @@ int minimum(int* grades, int gradeCount) {
 		}
 	}
 	return minimum;
+}
+
+double average(int* grades, int gradeCount) {
+	int sum = 0;
+	for (int i{ 0 }; i < gradeCount; i++) {
+		sum += grades[i];
+	}
+	return static_cast<double>(sum) / gradeCount;
 }
