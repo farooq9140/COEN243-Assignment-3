c24bb8415c79ec0882442d5dc93ae8d888ba8f41
Kevin <promkev@gmail.com>
2019-10-26 14:49:32 -0400

    Began question 3

---
+++ Question 3/Question 3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void controllerMenu();
+
+int main() {
+	return 0;
+}
+
+void controllerMenu() {
+	cout << "Controller Menu: " << endl << "1. Right" << endl << " 2. Left" << endl << "3. Display" << endl << "4. Reboot" << endl << "5. Show Array" << endl <<  "6. Exit" << endl;
+}
